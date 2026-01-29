a938bb88b349448448e6de2e0378096bbf7cb4ea
nithyaparaman12-pixel <nithyaparaman12@gmail.com>
2026-01-29 22:16:22 +0530

    Create Calculator using loop.py

---
+++ Python-basics/Calculator using loop.py
@@ -0,0 +1,30 @@
+while True:
+    print("\n--- Advanced Calculator ---")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = int(input("Enter your choice (1-5): "))
+
+    if choice == 5:
+        print("Calculator closed")
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == 1:
+        print("Result:", num1 + num2)
+    elif choice == 2:
+        print("Result:", num1 - num2)
+    elif choice == 3:
+        print("Result:", num1 * num2)
+    elif choice == 4:
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Cannot divide by zero")
+    else:
+        print("Invalid choice")
