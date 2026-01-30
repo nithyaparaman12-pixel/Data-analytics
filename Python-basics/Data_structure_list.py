7ae305765143c9b85b35255502e804e40ca7e991
Nithya <nithyaparaman12@gmail.com>
2026-01-30 12:33:30 +0530

    Create Data_structure_list.py

---
+++ Python-basics/Data_structure_list.py
@@ -0,0 +1,14 @@
+items_to_buy = ["apple","orange","banana","straberry","pineapple"]
+#printing list
+items_to_buy()
+#output ["apple","orange","banana","straberry","pineapple"]
+
+#slicing_positive_indexing 
+items_to_buy[0:2]
+#output["apple","orange"]
+#slicing_negative_indexing
+items_ti_buy[-1:-4]
+#output["orange","banana","straberry","pineapple"]
+#replacing using position indexing
+items_to_buy[3]= "peache"
+#output ["apple","orange","banana","peache","pineapple"]
